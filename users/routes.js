9131685090dd1dcaecba1385bca67c2f29157a1e
jesse h <jesselevihall@gmail.com>
2022-04-11 11:38:49 -0600

    user password encription

--- users/routes.js
+++ users/routes.js
@@ -1,32 +1,40 @@
 const router = require("express").Router();
 const UserModel = require("./model");
+const bcrypt = require("bcryptjs");
 
 router.post("/login", (req, res, next) => {
   res.send("Login");
 });
 
-router.post("/register", userRegValidation, emailInUse, (req, res, next) => {
-  const newUser = new UserModel({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  newUser
-    .save()
-    .then((document) => {
-      if (document) {
-        document.password = undefined;
-        res.json(document);
-      } else {
-        res.send("unable to create user");
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send("there was an error");
+router.post(
+  "/register",
+  userRegValidation,
+  emailInUse,
+  hashPassword,
+  (req, res, next) => {
+    console.log(req.body.password);
+    const newUser = new UserModel({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
     });
-});
+    newUser
+      .save()
+      .then((document) => {
+        if (document) {
+          document.password = undefined;
+          res.json(document);
+        } else {
+          res.send("unable to create user");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send("there was an error");
+      });
+  }
+);
 router.get("/:id", (req, res, next) => {
   UserModel.findById(req.params.id)
     .then((user) => {
@@ -82,4 +90,19 @@ function emailInUse(req, res, next) {
       res.status(500).send("there was an error");
     });
 }
+
+function hashPassword(req, res, next) {
+  const { password } = req.body;
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, function (err, passwordHash) {
+      if (err) {
+        res.status(500).send("there was an error");
+      } else {
+        req.body.password = passwordHash;
+        next();
+      }
+    });
+  });
+}
+
 module.exports = router;
